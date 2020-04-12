98a2561dde44e19a0d8360a1aaddc4eeede42b5b
mrvangle <1615893491@qq.com>
2020-04-12 11:34:44 +0800

    Site updated: 2020-04-12 11:34:43

--- js/click.js
+++ js/click.js
@@ -1,3 +1,5 @@
+const $ = require( 'jquery' );
+( function (click){g.
 !function(e,t,a){
     function n(){
         c(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}"),
@@ -40,3 +42,5 @@
     n()
 }
 (window,document);
+})($);
+module. exports
